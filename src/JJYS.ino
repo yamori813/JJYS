a92654ecbb4b39488db42f4be8ffb90ef3aea926
Hiroki Mori <yamori813@yahoo.co.jp>
2023-05-15 19:54:22 +0900

    revert error check

--- src/JJYS.ino
+++ src/JJYS.ino
@@ -251,18 +251,19 @@ void intr()
           }
         }
       }
-/*
       else {
-        if(debug) {
-          Serial.println("");
-          Serial.print("Error ");
-          Serial.println(aTime);
+        /* ignore callsine */
+        if(!((minutes == 15 || minutes == 45) && pos == 50)) {
+          if(debug) {
+            Serial.println("");
+            Serial.print("Error ");
+            Serial.println(aTime);
+          }
+//        if(jjystat == 2)
+//          MsTimer2::stop();
+          jjystat = 0;
         }
-        if(jjystat == 2)
-          MsTimer2::stop();
-        jjystat = 0;
       }
-*/
       startTime = nowTime;
       if(jjystat == 1) {
         ++pos;
